33c4afe602a0226a2067d9f22df31655bda4d899
Aaron Schlesinger <arschles@gmail.com>
2011-05-17 22:48:55 -0700

    implemented the record_exception method

--- lib/exception_mixin.rb
+++ lib/exception_mixin.rb
@@ -18,11 +18,21 @@ module Ripeline
   
   module Exception
     
+    @@mongo_Exceptions_coll = nil
     protected
     
     def record_exception e
-      raise "e must be an Exception subclass" if not e.is_a? Exception
-      #TODO: impl this
+      
+      raise "e must be an exception" if not e.is_a? Exception
+      
+      if @@mongo_exceptions_coll == nil
+        mongo_conn = Mongo::Connection.new
+        mongo_db = mongo_conn[:octodoc]
+        @@mongo_exceptions_coll = monbo_db["#{self.name}-exceptions"]
+      end
+      
+      @@mongo_exceptions_coll.insert :from => self.pipeline_identifier, :exception_message => e.message, :exception_backtrace => e.backtrace
+      
     end
   
   end
