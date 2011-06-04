4889e775e875a7efc8a6a7441a57c2bd658fb1ff
Aaron Schlesinger <arschles@gmail.com>
2011-06-03 23:24:07 -0700

    putting this one under the Ripeline::StageMixins module too

--- lib/stage/stats_mixin.rb
+++ lib/stage/stats_mixin.rb
@@ -12,19 +12,19 @@ end
 do_requires
 
 module Ripeline
-  
-  module Stats
+  module StageMixins
+    module Stats
     
-    def all_stats
-      @redis.hgetall self.stats_key
-    end
+      def all_stats
+        @redis.hgetall self.stats_key
+      end
     
-    protected
+      protected
     
-    def stat_count key
-      @redis.hincrby self.stats_hash_key, key, 1
-    end
+      def stat_count key
+        @redis.hincrby self.stats_hash_key, key, 1
+      end
       
+    end
   end
-  
 end
